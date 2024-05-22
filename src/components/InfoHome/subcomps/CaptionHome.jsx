fc574640ddbd4ef41eea54694f67355047d32594
Muhammad Malook <maharmohammadmalook@gmail.com>
2024-05-22 16:28:40 +0500

    ui fixes

--- src/components/InfoHome/subcomps/CaptionHome.jsx
+++ src/components/InfoHome/subcomps/CaptionHome.jsx
@@ -1,7 +1,7 @@
 const CaptionHome = () => {
   return (
     <h2 className="text-4xl text-[#192839] font-Nunito font-bold">
-      Products within reach of a click.
+      Products that are click-away.
     </h2>
   );
 };
